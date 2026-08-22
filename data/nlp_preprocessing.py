a20cc8c62db3aaa624ba1fdbbbe164f56ede020c
Pranav Bansal <143541416+PranavBansal7@users.noreply.github.com>
2026-08-22 21:15:42 +0530

    Update NLP preprocessing (NLP 2/4)

--- data/nlp_preprocessing.py
+++ data/nlp_preprocessing.py
@@ -10,10 +10,10 @@ class Solution:
         # 2. Encode each sentence by replacing words with their IDs
         # 3. Combine positive + negative into one list of tensors
         # 4. Pad shorter sequences with 0s using nn.utils.rnn.pad_sequence(tensors, batch_first=True)
-        combined = positive + negative
-        upos = sorted(set(word for sentence in combined for word in sentence.split()))
+        combined = [sentence.split() for sentence in (positive + negative)]
+        upos = sorted(set(word for sentence in combined for word in sentence))
         tab = {word:i for i,word in enumerate(upos,start=1)}
-        x = [torch.tensor([tab[word] for word in sentence.split()]) for sentence in combined]
+        x = [torch.tensor([tab[word] for word in sentence]) for sentence in combined]
         x = pad_sequence(x,batch_first=True,padding_value=0)
         return x.float()
 
